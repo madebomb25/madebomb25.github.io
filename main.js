81048c295f0b54914a40b355633c6144ec032bf5
TheDemonCoder25 <73224325+madebomb25@users.noreply.github.com>
2022-04-26 21:08:32 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+//Initial webpage scroll
+ScrollReveal().reveal('.cv-container', {delay:500});
+ScrollReveal().reveal('img', {delay:600});
+
+// Scroll of the right containers
+ScrollReveal().reveal('.education', {delay:700});
+ScrollReveal().reveal('.projects', {delay:700});
+ScrollReveal().reveal('.languages', {delay:700});
+ScrollReveal().reveal('.hobbies', {delay:700});
+
+//Scroll of the left containers
+ScrollReveal().reveal('.profile', {delay:800});
+ScrollReveal().reveal('.skills', {delay:800});
+ScrollReveal().reveal('.aptitudes', {delay:800});
+ScrollReveal().reveal('.contact', {delay:800});
+
+//Scroll of the smooth bar
+ScrollReveal().reveal('.smooth-bar', {delay:900});
